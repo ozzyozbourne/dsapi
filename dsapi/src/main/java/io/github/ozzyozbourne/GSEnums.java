49ae61cbea991da2fc3281fa9565154fe4f98447
ozzy ozbourne <khanosaid726@gmail.com>
2022-11-20 18:32:09 +0530

    setup done

---
+++ dsapi/src/main/java/io/github/ozzyozbourne/GSEnums.java
@@ -0,0 +1,12 @@
+package io.github.ozzyozbourne;
+
+public enum GSEnums {
+    FORMATTED_VALUE,
+    UNFORMATTED_VALUE,
+    FORMULA,
+    RAW,
+    USER_ENTERED,
+    INPUT_VALUE_OPTION_UNSPECIFIED,
+    SERIAL_NUMBER,
+    FORMATTED_STRING,
+}
---
+++ dsapi/src/main/java/io/github/ozzyozbourne/GSapi.java
@@ -0,0 +1,192 @@
+
+package io.github.ozzyozbourne;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.SheetsScopes;
+import com.google.api.services.sheets.v4.model.AppendValuesResponse;
+import com.google.api.services.sheets.v4.model.UpdateValuesResponse;
+import com.google.api.services.sheets.v4.model.ValueRange;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.GoogleCredentials;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+public class GSapi<T> {
+    private final String GOOGLE_SHEETS_ID;
+    private final String APPLICATION_NAME;
+    private final String TOKENS_DIRECTORY_PATH;
+    private final String CREDS_STORE;
+    private final JsonFactory JSON_FACTORY;
+    private final List<String> SCOPES;
+    private final NetHttpTransport netHttpTransport;
+    public  final Sheets sheetsService;
+    private final  Class<T> RESOURCE_CLASS;
+    private GSapi(Builder<T> builder)  {
+        this.GOOGLE_SHEETS_ID = builder.GOOGLE_SHEETS_ID;
+        this.APPLICATION_NAME = builder.APPLICATION_NAME;
+        this.RESOURCE_CLASS = builder.RESOURCE_CLASS;
+        this.TOKENS_DIRECTORY_PATH = builder.TOKENS_DIRECTORY_PATH;
+        this.CREDS_STORE = builder.CREDS_STORE;
+        this.SCOPES = builder.SCOPES;
+        this.JSON_FACTORY = GsonFactory.getDefaultInstance();
+        this.netHttpTransport = getNetHttpTransPort();
+        this.sheetsService = builder.IS_SERVICE_ACCOUNT?getDSService():getDSAuth();
+    }
+    private NetHttpTransport getNetHttpTransPort(){
+
+        NetHttpTransport netHttpTransport;
+        try {
+            netHttpTransport =  GoogleNetHttpTransport.newTrustedTransport();
+        } catch (GeneralSecurityException | IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+        return netHttpTransport;
+    }
+
+    public UpdateValuesResponse update(List<List<Object>> valueToBeUploaded, String locationInSheets) throws IOException {
+        return  sheetsService.spreadsheets()
+                .values()
+                .update(GOOGLE_SHEETS_ID, locationInSheets, new ValueRange().setValues(valueToBeUploaded))
+                .setValueInputOption(GSEnums.USER_ENTERED.name()).setIncludeValuesInResponse(true).execute();
+    }
+
+    public AppendValuesResponse append(List<List<Object>> valueToBeAppended, String locationInSheets) throws IOException {
+        return  sheetsService.spreadsheets()
+                .values()
+                .append(GOOGLE_SHEETS_ID, locationInSheets, new ValueRange().setValues(valueToBeAppended))
+                .setValueInputOption(GSEnums.USER_ENTERED.name()).setIncludeValuesInResponse(true).execute();
+    }
+
+    private Credential authorize(){
+
+        Credential credential;
+
+        try(InputStream inputStream = RESOURCE_CLASS.getResourceAsStream(CREDS_STORE)){
+
+            GoogleClientSecrets clientSecrets =
+                    GoogleClientSecrets.
+                            load(JSON_FACTORY, new InputStreamReader(Objects.requireNonNull(inputStream)));
+
+            GoogleAuthorizationCodeFlow flow  = new GoogleAuthorizationCodeFlow.Builder(
+                    netHttpTransport , JSON_FACTORY, clientSecrets, SCOPES)
+                    .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                    .setAccessType("offline")
+                    .build();
+
+            credential = new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+        return Objects.requireNonNull(credential);
+
+    }
+
+    private Sheets getDSAuth(){
+        Credential credential = authorize();
+        return new  Sheets.Builder(netHttpTransport, JSON_FACTORY, credential)
+                .setApplicationName(APPLICATION_NAME).build();
+    }
+
+
+    private Sheets getDSService(){
+        GoogleCredentials credentials = null;
+        try {
+            credentials = GoogleCredentials.fromStream(Objects.requireNonNull(RESOURCE_CLASS.getResourceAsStream(CREDS_STORE)));
+            credentials.createScoped(SCOPES).refreshIfExpired();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new  Sheets.Builder(netHttpTransport, JSON_FACTORY,
+                new HttpCredentialsAdapter(Objects.requireNonNull(credentials)))
+                .setApplicationName(APPLICATION_NAME).build();
+    }
+
+    public static class Builder<T>{
+
+        private  String GOOGLE_SHEETS_ID;
+
+        private final  Class<T> RESOURCE_CLASS;
+        private  String APPLICATION_NAME;
+        private  String TOKENS_DIRECTORY_PATH = "tokens";
+        private  String CREDS_STORE = "/credstore/creds.json";
+        private  List<String> SCOPES = Collections.singletonList(SheetsScopes.SPREADSHEETS);
+        private boolean IS_SERVICE_ACCOUNT = true;
+
+
+        public static <T> Builder<T> getBuilder(Class<T> tClass){
+            return new Builder<>(tClass);
+        }
+
+        public GSapi<T> build()
+        {
+            return new GSapi<>(this);
+        }
+
+        public Builder<T> setGOOGLE_SHEETS_ID(String GOOGLE_SHEETS_ID) {
+            this.GOOGLE_SHEETS_ID = GOOGLE_SHEETS_ID;
+            return this;
+        }
+
+        private Builder(Class<T> tClass) {
+            this.RESOURCE_CLASS = tClass;
+        }
+
+        public Builder<T> setAPPLICATION_NAME(String APPLICATION_NAME) {
+            this.APPLICATION_NAME = APPLICATION_NAME;
+            return this;
+        }
+
+        public Builder<T> setTOKENS_DIRECTORY_PATH(String TOKENS_DIRECTORY_PATH) {
+            this.TOKENS_DIRECTORY_PATH = TOKENS_DIRECTORY_PATH;
+            return this;
+        }
+
+        public Builder<T> setCREDS_STORE(String CREDS_STORE) {
+            this.CREDS_STORE = CREDS_STORE;
+            return this;
+        }
+
+        public Builder<T> setSCOPES(List<String> SCOPES) {
+            this.SCOPES = SCOPES;
+            return this;
+        }
+
+        public Builder<T> IS_SERVICE_ACCOUNT(boolean bool) {
+            this.IS_SERVICE_ACCOUNT = bool;
+            return this;
+        }
+
+
+
+}
+    @Override
+    public String toString() {
+        return "GoogleSheets{" +
+                "GOOGLE_SHEETS_ID='" + GOOGLE_SHEETS_ID + '\'' +
+                ", APPLICATION_NAME='" + APPLICATION_NAME + '\'' +
+                ", TOKENS_DIRECTORY_PATH='" + TOKENS_DIRECTORY_PATH + '\'' +
+                ", CREDS_STORE='" + CREDS_STORE + '\'' +
+                ", JSON_FACTORY=" + JSON_FACTORY +
+                ", SCOPES=" + SCOPES +
+                '}';
+    }
+}
